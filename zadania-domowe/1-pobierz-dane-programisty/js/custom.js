8de6b839ea819823b8a9ebaae2247d18d330beb2
Aleksandra Hannum <aleksandrahannum@gmail.com>
2017-10-30 10:57:45 +0100

    dodano div

--- zadania-domowe/1-pobierz-dane-programisty/js/custom.js
+++ zadania-domowe/1-pobierz-dane-programisty/js/custom.js
@@ -4,6 +4,11 @@ $(function() {
     $('button').click(function() {
         $.getJSON('https://akademia108.pl/kurs-front-end/ajax/1-pobierz-dane-programisty.php', function (data) {
             console.log(data);
+//            $('div').append('data');
+            var div = document.createElement('div');
+            div.innerHTML = data.name;
+            
+            document.body.appendChild(div);
         })
     })
 });
@@ -20,7 +25,10 @@ $(function() {
 //
 //            console.log(data);
 //
+//var par = document.createElement('p');
+//                        par.innerHTML = element.name;
 //
+//                        document.body.appendChild(par);
 //
 //
 //        });
