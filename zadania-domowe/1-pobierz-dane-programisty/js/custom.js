b64e1fe58c5cb19bd0b5a729f85499969510bdb5
Aleksandra Hannum <aleksandrahannum@gmail.com>
2017-10-30 11:25:45 +0100

    zmieniono strukture

--- zadania-domowe/1-pobierz-dane-programisty/js/custom.js
+++ zadania-domowe/1-pobierz-dane-programisty/js/custom.js
@@ -3,34 +3,11 @@
 $(function() {
     $('button').click(function() {
         $.getJSON('https://akademia108.pl/kurs-front-end/ajax/1-pobierz-dane-programisty.php', function (data) {
-            console.log(data);
-//            $('div').append('data');
-            var div = document.createElement('div');
-            div.innerHTML = data.name;
-            
-            document.body.appendChild(div);
-        })
-    })
+//            console.log(data);
+            var dane = $("<div></div>" + data.imie + "<br>" + data.nazwisko + "<br>" + data.zawod + "<br>" + data.firma);
+            $("#dane-programisty").append(dane);
+        
+        });
+    });
 });
 
-
-
-
-
-
-
-//
-//$('button').click(function() {
-//        $.getJSON('http://echo.jsontest.com/userId/108/userName/Akademia108/userURL/akademia108.pl', function (data) {
-//
-//            console.log(data);
-//
-//var par = document.createElement('p');
-//                        par.innerHTML = element.name;
-//
-//                        document.body.appendChild(par);
-//
-//
-//        });
-//        
-//    });
