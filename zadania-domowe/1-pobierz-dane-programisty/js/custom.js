cb98dead252b518ef7ae878c26ea66b7681acb4f
Aleksandra Hannum <owojcick@gmail.com>
2017-10-31 11:26:28 +0100

    doano wyświetlanie w przeglądarce

--- zadania-domowe/1-pobierz-dane-programisty/js/custom.js
+++ zadania-domowe/1-pobierz-dane-programisty/js/custom.js
@@ -1,13 +1,16 @@
 'use strict';
 
-$(function() {
-    $('button').click(function() {
+$(function () {
+    $('button').click(function () {
         $.getJSON('https://akademia108.pl/kurs-front-end/ajax/1-pobierz-dane-programisty.php', function (data) {
-//            console.log(data);
-            var dane = $("<div></div>" + data.imie + "<br>" + data.nazwisko + "<br>" + data.zawod + "<br>" + data.firma);
-            $("#dane-programisty").append(dane);
-        
+            console.log(data);
+            
+            var dane = document.createElement('div');
+
+            dane.innerHTML = 'Imię: ' + data.imie + '<br>' + 'Nazwisko: ' + data.nazwisko + '<br>' + 'Zawód: ' + data.zawod + '<br>' + 'Frima: ' + data.firma;
+            
+            document.body.appendChild(dane);
+
         });
     });
 });
-
