202d58db236ff9c692ee45309783776dacf8fd3e
Aleksandra Hannum <owojcick@gmail.com>
2017-11-06 19:32:47 +0100

    dodano

---
+++ zadania-domowe/portfolio-bootstrap/js/custom.js
@@ -0,0 +1,15 @@
+$(function () {
+
+    if ($(window).scrollTop() >= 50) {
+        $('#main-nav').addClass('scroll');
+    };
+
+    $(window).scroll(function () {
+        if ($(window).scrollTop() >= 50) {
+            $('#main-nav').addClass('scroll');
+        } else {
+            $('#main-nav').removeClass('scroll');
+        }
+
+    });
+});
